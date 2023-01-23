9d59610db525f74404e8e94b665a21e4ecff05d9
ozobianwadibia <Ozobia.cn3@gmail.com>
2023-01-23 14:25:54 -0600

    Completed the 'Remove spaces' kata

---
+++ 8kyu/RemoveSpaces.java
@@ -0,0 +1,8 @@
+//Simple, remove the spaces from the string, then return the resultant string.
+
+
+public class RemoveSpaces {
+    public static String noSpace(final String x) {
+        return x.replaceAll(" ", "");
+    }
+}
