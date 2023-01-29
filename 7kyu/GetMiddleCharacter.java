f31cd4aeab93b55a44793f4d5f0f1f22de7a27f8
ozobianwadibia <Ozobia.cn3@gmail.com>
2023-01-28 22:12:15 -0600

    Completed the 'GetMiddleCharacter' kata

---
+++ 7kyu/GetMiddleCharacter.java
@@ -0,0 +1,44 @@
+//You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+//
+//        #Examples:
+//
+//        Kata.getMiddle("test") should return "es"
+//
+//        Kata.getMiddle("testing") should return "t"
+//
+//        Kata.getMiddle("middle") should return "dd"
+//
+//        Kata.getMiddle("A") should return "A"
+//        #Input
+//
+//        A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+//
+//        #Output
+//
+//        The middle character(s) of the word represented as a string
+
+
+public class GetMiddleCharacter {
+    public static String getMiddle(String word) {
+
+        // for a string with a length of 1
+        if (word.length() == 1) {
+            return word;
+        }
+
+        // for a string with an odd length
+        if (word.length() % 2 != 0) {
+            int index = (int)(Math.round(word.length() / 2.0));
+            String firstPart = word.substring(0, index);
+            return (firstPart.substring(firstPart.length() - 1));
+        }
+        // for a string with an even length
+        int index = (word.length() / 2);
+        String firstPart = word.substring(0, index);
+        firstPart = firstPart.substring(firstPart.length() - 1); // ans
+        String secondPart = word.substring(index);
+        secondPart = String.valueOf(secondPart.charAt(0));
+        return firstPart + secondPart;
+    }
+
+}
