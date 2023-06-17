dd7a910c268a3c775dc1dd309381454c49690f7f
ozobianwadibia <Ozobia.cn3@gmail.com>
2023-06-17 16:26:51 -0500

    Completed the 'Banjo' kata

---
+++ 8kyu/Banjo.java
@@ -0,0 +1,23 @@
+
+//Create a function which answers the question "Are you playing banjo?".
+//        If your name starts with the letter "R" or lower case "r", you are playing banjo!
+//
+//        The function takes a name as its only argument, and returns one of the following strings:
+//
+//        name + " plays banjo"
+//        name + " does not play banjo"
+//
+//        Names given are always valid strings.
+
+public class Banjo {
+
+    public static String areYouPlayingBanjo(String name) {
+        // Program me!
+        if (name.charAt(0) == 'R' || name.charAt(0) == 'r') {
+            return name + " plays banjo";
+        }
+        else {
+            return name + " does not play banjo";
+        }
+    }
+}
